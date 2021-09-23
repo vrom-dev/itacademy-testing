165961c9e5e647f82c07f8dde2a3470e81855be6
Víctor Romero <vromerocomino@gmail.com>
2021-09-23 12:58:51 +0200

    Initial commit

---
+++ app/nivell1/calculator.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b
+const substract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => a / b
+
+module.exports = {
+  sum,
+  substract,
+  multiply,
+  divide
+}
